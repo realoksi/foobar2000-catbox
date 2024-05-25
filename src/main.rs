e82ee403475feb32907a26055cfe0fa7abfe9cdd
realoksi <realoksi@pm.me>
2024-05-25 17:20:26 -0400

    logic for a line separated configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::{
+    collections::HashMap,
+    env,
+    fs::File,
     io::{self, BufRead, Cursor},
     path::Path,
     process::exit,
@@ -26,12 +29,60 @@ mod error {
 }
 
 fn main() {
-    const URL: &str = "https://catbox.moe/user/api.php";
-    const USER_AGENT: &str =
-        "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0";
     // const THRESHOLD: usize = 2097152; // 2MiB
-    const CONSTRAIN: u32 = 500; // Image height/width to always constrain to.
-    const QUALITY: u8 = 80; // Image quality percentage
+
+    // Configuration map initialization
+
+    let config_path: &Path = &env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("config.txt");
+    let mut config: HashMap<String, String> = HashMap::new();
+
+    if let Ok(config_file) = File::open(&config_path) {
+        let config_reader: io::BufReader<File> = io::BufReader::new(config_file);
+
+        for line in config_reader.lines() {
+            if let Ok(line) = line {
+                if let Some((key, value)) = line.split_once("=") {
+                    config.insert(key.trim().to_string(), value.trim().to_string());
+                }
+            }
+        }
+    }
+
+    // Definitions for default values
+
+    const DEFAULT_MAX_WIDTH: u32 = 500;
+    const DEFAULT_MAX_HEIGHT: u32 = 500;
+    const DEFAULT_QUALITY: u8 = 80;
+    let default_user_agent: String =
+        "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0".to_string();
+    let default_endpoint: String = "https://catbox.moe/user/api.php".to_string();
+
+    // Configuration value initialization
+    // When a value isn't available or is invalid, these will always default to their appropriate hardcoded value above.
+
+    let max_width: u32 = config
+        .get("MAX_WIDTH")
+        .unwrap_or(&DEFAULT_MAX_WIDTH.to_string())
+        .parse()
+        .unwrap_or(DEFAULT_MAX_WIDTH);
+    let max_height: u32 = config
+        .get("MAX_HEIGHT")
+        .unwrap_or(&DEFAULT_MAX_HEIGHT.to_string())
+        .parse()
+        .unwrap_or(DEFAULT_MAX_HEIGHT);
+    let quality: u8 = config
+        .get("QUALITY")
+        .unwrap_or(&DEFAULT_QUALITY.to_string())
+        .parse()
+        .unwrap_or(DEFAULT_QUALITY);
+    let user_agent: &String = config.get("USER_AGENT").unwrap_or(&default_user_agent);
+    let endpoint: &String = config.get("ENDPOINT").unwrap_or(&default_endpoint);
+
+    // ...
 
     let input: String = io::stdin()
         .lock()
@@ -63,15 +114,15 @@ fn main() {
         });
 
     let resize_buffer: image::DynamicImage =
-        if image_buffer.width() > CONSTRAIN || image_buffer.height() > CONSTRAIN {
-            image_buffer.resize(CONSTRAIN, CONSTRAIN, image::imageops::FilterType::Nearest)
+        if image_buffer.width() > max_width || image_buffer.height() > max_height {
+            image_buffer.resize(max_width, max_height, image::imageops::FilterType::Nearest)
         } else {
             image_buffer
         };
 
     let mut cursor: Cursor<Vec<u8>> = Cursor::new(Vec::new());
 
-    JpegEncoder::new_with_quality(&mut cursor, QUALITY)
+    JpegEncoder::new_with_quality(&mut cursor, quality)
         .encode_image(&resize_buffer)
         .unwrap_or_else(|_| {
             eprintln!("Failed to encode image");
@@ -90,11 +141,11 @@ fn main() {
     let mut headers: List = List::new();
     headers.append("Content-Type: multipart/form-data").unwrap();
     headers
-        .append(format!("User-Agent: {}", USER_AGENT).as_str())
+        .append(format!("User-Agent: {}", user_agent).as_str())
         .unwrap();
 
     let mut easy: Easy2<ResponseBody> = Easy2::new(ResponseBody(Vec::new()));
-    easy.url(URL).unwrap();
+    easy.url(endpoint).unwrap();
     easy.http_headers(headers).unwrap();
     easy.httppost(form).unwrap();
 
